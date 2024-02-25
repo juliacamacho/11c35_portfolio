90151aa352ac799ab974411e72b22faa894f5e8c
Julia Camacho <juliac123@gmail.com>
2024-02-25 16:46:54 -0500

    add js and update paths

---
+++ global.js
@@ -0,0 +1,13 @@
+console.log("IT’S ALIVE!");
+
+function $$ (selector, context = document) {
+	return Array.from(context.querySelectorAll(selector));
+}
+
+// navLinks = $$("nav a")
+// let currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname)
+
+// if (currentLink) { // or if (currentLink !== undefined)
+// 	currentLink.classList.add("current");
+// }
+

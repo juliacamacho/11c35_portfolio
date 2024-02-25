c1ea5d5960c0c92706d29b9678792010b20dc31a
Julia Camacho <juliac123@gmail.com>
2024-02-25 16:57:54 -0500

    update nav

--- global.js
+++ global.js
@@ -11,3 +11,26 @@ function $$ (selector, context = document) {
 // 	currentLink.classList.add("current");
 // }
 
+let pages = [
+	{url: "", title: "Home"},
+	{url: "projects/", title: "Projects"},
+	{url: "contact/", title: "Contact"},
+	{url: "cv/", title: "CV"},
+	{url: "https://github.com/juliacamacho", title: "Github"}
+];
+
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+const ARE_WE_HOME = document.documentElement.classList.contains("home");
+
+for (let p of pages) {
+	let url = p.url;
+	url = !ARE_WE_HOME && !url.startsWith("http") ? "../" + url : url;
+	let title = p.title;
+
+	// Create link and add it to nav
+	nav.insertAdjacentHTML("beforeend", `<a href="${ url }">${ title }</a>` );
+}
+
+
